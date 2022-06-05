390306028578aaab6417f7b7a09b912f369b09b7
limpido <jiichuan0325@gmail.com>
2022-06-06 00:06:17 +0800

    Bug fix: chatroom page fix unique id

--- src/pages/RobertChatroom/Chatroom.js
+++ src/pages/RobertChatroom/Chatroom.js
@@ -19,11 +19,9 @@ export default function Chatroom() {
       <div className="flex justify-center items-center h-screen mx-4">
         <div className='flex flex-col'>
           <div className="border-2 border-indigo-500 rounded-t-md bg-primary p-4 w-128 overflow-y-scroll flex flex-col h-128 max-w-7xl min-w-7xl">
-            {messages.map(({ id, text, uid }) => (
-              <div>
-                <div key={id} className={`flex p-5 space-x-1 items-center my-2 rounded-full ${uid === auth.currentUser.uid ? 'rounded-br-3xl bg-[#37257f] flex-row-reverse float-right text-white' : 'rounded-bl-3xl bg-[#fafafa] float-left'}`}>
-                  <p className='font-medium text-md break-words'>{text}</p>
-                </div>
+            {messages.map(({ text, uid }, idx) => (
+              <div key={idx} className={`flex p-5 space-x-1 items-center my-2 rounded-full ${uid === auth.currentUser.uid ? 'rounded-br-3xl bg-[#37257f] flex-row-reverse float-right text-white' : 'rounded-bl-3xl bg-[#fafafa] float-left'}`}>
+                <p className='font-medium text-md break-words'>{text}</p>
               </div>
             ))}
           </div>
